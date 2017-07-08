e89576ab4b63fdb25300738838f6be516afc1c25
MarDiKernel <marcosnintendo18@gmail.com>
2017-07-08 14:21:40 +0200

    Update Primos.c

--- Primos.c
+++ Primos.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+int main (void)
+{
+  int n, n2, n_hasta, divisores, n_option, numero;
+  printf("\n\nHasta que número quieres mostrar: ");
+  scanf("%d", &n_hasta);
+  
+  printf("\n¿Desde donde quieres que comienze?: ");
+  printf("\n\n\t1. El 1");
+  printf("\n\t2. Otro");
+  printf("\n\nElige una opción(1/2): ");
+  scanf("%d", &n_option);
+  
+  if (n_option == 1){
+    n=2;
+  }
+  if (n_option == 2){
+    printf("\nNúmero de Inicio [Mayor que 1]: ");
+    scanf("%d", &numero);
+    n=numero;
+  }
+  
+  while 
+}
