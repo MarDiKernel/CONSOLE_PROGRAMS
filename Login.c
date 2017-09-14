0d04a2471fc9dc886739c5993bca4b04d9d7e03f
MarDiKernel <marcosnintendo18@gmail.com>
2017-09-14 19:07:31 +0200

    Update Login.c

--- Login.c
+++ Login.c
@@ -2,46 +2,59 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define USER "usuario" // Definir el nombre correcto para el usuario
-#define PASSWD "12357" // Contraseña para el usuario
+#define USER "root" // Definir el nombre correcto para el usuario
+#define PASSWD "toor123" // Contraseña para el usuario
 
 int main(void)
 {
   char user[16];
   char passwd[16];
-  int n_user, n_passwd;
+  int n_passwd, veces_user, veces_passwd;
   
-  n_user=0;
   n_passwd=0;
-  
-  while (n_user == 0)
-  {
-    printf("\nUSUARIO: ");
-     scanf("%s", &user);
-     fflush(stdin);
-     if (strcasecmp (user, USER) == 0){
-      n_user=1;
-     }
-     else{
-      system("clear");
-     }
-  }
+  veces_user=0;
+  veces_passwd=0;
   
   while (n_passwd == 0)
   {
     system("clear");
-    printf("\nUSUARIO: %s\nCONTRASEÑA: ", user);
-    scanf("%s", &passwd);
+    printf("\nUSER: "); //Poner el el idioma que se quiera
+    gets(user);
     fflush(stdin);
-    if (strcmp (passwd, PASSWD) == 0){
+    if (strcasecmp (user, USER) == 0){
       n_passwd=1;
+      while (n_passwd == 1)
+      {
+        system("clear");
+        printf("\nUSER: %s\nPASSWORD: ", user); //Poner en el idioma que se quiera
+        gets(passwd);
+        fflush(stdin);
+        if (strcmp (passwd, PASSWD) == 0){
+          n_passwd=2;
+        }
+        else{
+          veces_passwd++;
+          if (veces_passwd == 3){
+            n_passwd=3;
+          }
+        }
+      }
+    }
+    else{
+      veces_user++;
+      if (veces_user == 3){
+        n_passwd=3;  
+      }
     }
   }
-  fflush(stdin);
-  getchar();
-  printf("\n\n\tACCESO CONCEDIDO [ %s ]", user);
-  printf("\n\nPulsa Intro para finalizar...\n");
-  getchar();
+  if (n_passwd == 3){
+    system("clear");
+    printf("\n\tACCESO DENEGADO\n");
+  }
+  if (n_passwd == 2){
+    printf("\n\tAcceso Concedido\n\n");
+    system("bash"); //Cambiar por la shell a usar
+  }
   
   return 0;
 }
