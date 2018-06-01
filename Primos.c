8d8f4dd71d9dfdca5713432e64d75dedc33e0ee5
MarDiKernel <marcosnintendo18@gmail.com>
2018-06-02 00:02:13 +0200

    Update Primos.c

--- Primos.c
+++ Primos.c
@@ -1,3 +1,8 @@
+/*[Primos.c] [v. 0.6] by MarDiKernel 
+  diseñado para sistemas UNIX.
+  
+  Última modificación 1/6/2018*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -5,15 +10,19 @@
 int main ()
 {
   int n, nhasta, npri, divisores, raiz, position, option;
-  char sn;
-  sn='S';
+  char sn; //Carácter que almacena la respuesta de volver a ejecutar el programa (S/N).
+  sn='S'; //Inicialización de la variable para que sea verdadera.
   
   do
   {
-	  position=0;
-	  system("clear");
+	  position=0; // La posición a 0.
+	  system("clear"); // Borra la pantalla. (La función "system", pertenece a la biblioteca stdlib.h).
 	  
-	  printf("\n\tIntervalo de números primos a mostrar[n1 - n2]: ");
+	  /*Ahora procedemos a la introducción del intervalo (cerrado) de números entre los cuales queremos calcular
+	  los primos existentes. Los corchetes son como unas comillas, no hay que ponerlos, únicamente indican la forma de los
+	  datos a introducir.*/
+	  
+	  printf("\n\tIntervalo de números primos a mostrar[n1 - n2]: "); 
 	  scanf("%d - %d", &n, &nhasta);
 	  fflush(stdin);
 	  printf("\n\n\t1. Mostrar solo los números primos.\n\t2. Mostrar todos los números.\n");
@@ -30,6 +39,25 @@ int main ()
 					++divisores;
 			}
 			switch (option)
+			{
+				case 1:
+					if (divisores == 1){
+						++position;
+						printf("\n\tPosición <%6d> -> [%8d]", position, n);
+						++n;
+					}
+					break;
+				case 2:
+					if (divisores == 1){
+						++position;
+						printf("\n\tPosición <%6d> -> [%8d]", position, n);
+						++n;
+					}
+					else{
+						printf(" %d,", n);
+					}
+					break;
+			}
 	  	}
 	  }
 	  else{
