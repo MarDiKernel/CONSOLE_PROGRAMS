6a9c7dc73068e3a82afd91efdbbfc02c10918ab5
MarDiKernel <marcosnintendo18@gmail.com>
2018-06-01 23:30:54 +0200

    Update Primos.c

--- Primos.c
+++ Primos.c
@@ -1,1 +1,20 @@
-#include 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main ()
+{
+  int n, nhasta, npri, divisores, raiz, position;
+  char sn;
+  sn='S';
+  
+  do
+	{
+		
+  }while ((sn == 'S')||(sn == 's'));
+  
+  printf("\n\nPulsa Intro para finalizar...\n");
+  getchar();
+  
+  return 0;
+}
